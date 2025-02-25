db472e3865329220a05485a845d3548ae2692c75
Ginger <75683114+gingershaped@users.noreply.github.com>
2025-02-25 13:01:21 -0500

    Add a basic REPL

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 use std::{error::Error, fs::read_to_string, path::PathBuf, rc::Rc};
 
+use anstream::println;
 use clap::{Args, Parser};
-use ferralisp::{machine::{Machine, World}, parser::parse};
+use ferralisp::{machine::{Machine, World}, parser::{parse, parse_expression}};
+use owo_colors::OwoColorize;
+use rustyline::{error::ReadlineError, DefaultEditor};
 
 #[derive(Debug, Parser)]
 #[command(name = "ferralisp")]
 struct Cli {
     #[command(flatten)]
-    source: Source,
+    source: Option<Source>,
 
     #[arg(long)]
     parsetree: bool,
 }
 
 #[derive(Debug, Args)]
-#[group(required = true, multiple = false)]
+#[group(multiple = false)]
 struct Source {
     #[arg(short = 'c', long = "code")]
     code: Option<String>,
@@ -37,26 +40,66 @@ impl World for ConsoleWorld {
     }
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let args = Cli::parse();
-    let source = args.source.read();
-
-    let parsed = parse(&source)?;
-    if args.parsetree {
-        println!("{:?}", parse(&source));
-        return Ok(());
-    }
-
+fn repl() -> Result<(), Box<dyn Error>> {
     let mut machine = Machine::with_default_loaders(ConsoleWorld);
-    for expression in parsed {
-        match machine.eval(Rc::new(expression.into())) {
-            Ok(value) => println!("{}", value),
-            Err(err) => {
-                println!("error!! {}", err);
+    let mut rl = DefaultEditor::new()?;
+
+    println!("(welcome to {})", "ferralisp".purple());
+    loop {
+        match rl.readline("tl> ") {
+            Ok(line) => {
+                match parse_expression(&line) {
+                    Ok(expression) => {
+                        match machine.eval(Rc::new(expression.into())) {
+                            Ok(value) => {
+                                println!("{}", value);
+                            },
+                            Err(error) => {
+                                println!("{}: {}", "error!!".bold().red(), error);
+                            }
+                        }
+                    },
+                    Err(error) => {
+                        println!("{}: {}", "parse error!!".bold().red(), error);
+                    },
+                }
+            },
+            Err(ReadlineError::Interrupted) => (),
+            Err(ReadlineError::Eof) => {
                 break;
             },
+            Err(_) => (),
         }
     }
-
     Ok(())
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::parse();
+    
+    match args.source {
+        Some(source) => {
+            let source = source.read();
+
+            let parsed = parse(&source)?;
+            if args.parsetree {
+                println!("{:?}", parse(&source));
+                return Ok(());
+            }
+
+            let mut machine = Machine::with_default_loaders(ConsoleWorld);
+            for expression in parsed {
+                match machine.eval(Rc::new(expression.into())) {
+                    Ok(value) => println!("{}", value),
+                    Err(err) => {
+                        println!("error!! {}", err);
+                        break;
+                    },
+                }
+            }
+
+            Ok(())
+        },
+        None => repl(),
+    }
 }
